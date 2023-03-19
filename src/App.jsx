ec66f9bf4659e36d64671413dff5a2c89cab36a5
PrimeFord <bashfordbash@gmail.com>
2023-03-19 12:29:13 +0100

    feat: Redux Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { decrease, increase, reset } from "./redux/action";
 
 function App() {
   const [count, setCount] = useState(0);
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="h-[75%] w-[45%] bg-white text-center">
+        <h1 className="text-[3rem] font-[400] mb-8">COUNTER</h1>
+        <p className="text-[5rem] font-[500] mb-8">{counter}</p>
+        <div className="w-[50%] flex justify-around m-auto">
+          <button
+            className="border-solid border-2 border-slate-400 text-black py-1 px-2 bg-[#c1c1c194] font-[400] {}"
+            onClick={() => {
+              dispatch(increase());
+            }}
+          >
+            Increase
+          </button>
+          <button
+            className="border-solid border-2 border-slate-400 text-black py-1 px-2 bg-[#c1c1c194] font-[400]"
+            onClick={() => {
+              dispatch(decrease());
+            }}
+          >
+            Decrease
+          </button>
+          <button
+            className="border-solid border-2 border-slate-400 text-black py-1 px-2 bg-[#c1c1c194] font-[400]"
+            onClick={() => {
+              dispatch(reset());
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from "./App";
+import "./index.css";
+import { counterReducer } from "./redux/reducer";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const store = createStore(counterReducer);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
